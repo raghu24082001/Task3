6e1985dc528e94245b504e192b458c3abb6a0824
raghu24082001 <raghudae248@gmail.com>
2026-07-05 20:31:42 +0530

    update2

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default axiosInstance;
+export default api;
